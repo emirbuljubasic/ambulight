cdc85b7ae03bd7183718b83b4895a9b155c8c5dd
Emir Buljubasic <emirbuljubasic329@gmail.com>
2024-02-11 03:54:18 +0100

    VOZILOOOOO

--- vozilo/main.c
+++ vozilo/main.c
@@ -17,24 +17,8 @@
 #define T2 3e-3
 #define T3 4e-3
 
-#define IR_50 500
-#define IR_25 250
-#define IR_MARGIN_0 150
-#define IR_MARGIN_1 350
-#define IR_MARGIN_2 600
-#define PERIOD_MARGIN_LOW 900
-#define PERIOD_MARGIN_HIGH 1100
-
 void init_TIM3();
 void init_DMA1(void);
-
-volatile int irq_counter = 0;
-uint16_t pwm_recieved_bits = 0x0000;
-uint16_t ambulance_id = 0xCA3A;
-
-uint8_t pwm_cnt = 0;
-uint8_t pwm_arr_size = 16;
-
 uint16_t pwm[16] = {IR_25, IR_50, IR_25, IR_50, IR_50, IR_50,
                     IR_25, IR_25, IR_25, IR_50, IR_25, IR_50,
                     IR_25, IR_25, IR_50, IR_50}; // LSB FIRST!!
@@ -43,9 +27,7 @@ uint8_t lights_on = 0;
 void getData4DAC(uint16_t *dac_buff, uint8_t type);
 
 /// IR LED:             +(anode) PB6 |^| GND (cathode)-
-/// IR PHOTODIODE:      +(anode) +5V |^| PA8 (chatode)-
-
-void init_TIM1();
+/// IR PHOTODIODE:      -(cathode) +5V |^| PA8 (anode)+
 
 int main(void) {
   uint16_t dac_buff1[DAC_BUFF_SIZE];
@@ -54,7 +36,6 @@ int main(void) {
   uint32_t k = 0;
   int temp = 0;
 
-  init_TIM1();
   initUSART2(USART2_BAUDRATE_921600);
 
   initDAC1();
@@ -84,23 +65,6 @@ int main(void) {
 
   while (1) {
     //
-
-    if (irq_counter == pwm_arr_size) {
-      // GPIOC->ODR |= 0x0004;
-      // TIM8->CNT |= 0xFFFF;
-      // GPIOD->ODR |= 0x8000;
-      // TIM8->CNT |= 0xFFFF;
-      irq_counter = 0;
-      // pwm_recieved_bits = 0x0000;
-
-      printUSARTMOJ("GREAT SUCCESS!\n");
-      printUSARTMOJ("%xw\n", pwm_recieved_bits);
-      printUSARTMOJ("%xw\n\n", ambulance_id);
-      pwm_recieved_bits = 0x0000;
-      // irq_counter = 0;
-      // time = getSYSTIMER();
-      // printUSART2("TIME = %xw\n\n", time);
-    }
 
     if ((GPIOA->IDR & 0x0001) == 0x0001) {
       // printUSART2("Button pressed!\n");
@@ -243,72 +207,4 @@ void getData4DAC(uint16_t *dac_buff, uint8_t type) {
     dac_buff[n] = (uint16_t)(y * 4095);
     t = t + 0.5e-4;
   }
-}
-
-void init_TIM1() {
-  // Initializing PA8 as TIM8 Channel 1
-  RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN;
-  GPIOA->MODER &= ~GPIO_MODER_MODER8;
-  GPIOA->MODER |= GPIO_MODER_MODER8_1;
-  GPIOA->AFR[1] |= 0x00000001;
-  GPIOA->PUPDR &= ~GPIO_PUPDR_PUPDR8;
-  GPIOA->PUPDR |= GPIO_PUPDR_PUPDR8_1;
-
-  // Initializing TIM1 in input PWM mode
-  RCC->APB2ENR |= RCC_APB2ENR_TIM1EN;
-  TIM1->PSC = 0x20D0 - 0x0001; // psc = 8400 -> clock: 20000
-  TIM1->ARR = 0x07D0;          // arr = 2000 period: 100ms
-  // TIM1->PSC = 0x20D0 - 0x0001;        // psc = 8400 -> clock: 20000
-  // TIM1->ARR = 0x4E20;                 // arr = 20000 period: 1000ms
-  TIM1->CR1 = 0x0000;
-  TIM1->CR2 = 0x0000;
-
-  TIM1->CCMR1 &= ~(TIM_CCMR1_CC1S);
-  TIM1->CCMR1 &= ~(TIM_CCMR1_CC2S);
-  TIM1->CCMR1 |= (TIM_CCMR1_CC1S_0) | (TIM_CCMR1_CC2S_1);
-
-  TIM1->CCER &= ~(TIM_CCER_CC1P);
-  TIM1->CCER &= ~(TIM_CCER_CC1NP);
-  TIM1->CCER &= ~(TIM_CCER_CC2NP);
-  TIM1->CCER |= (TIM_CCER_CC2P);
-  TIM1->CCER |= (TIM_CCER_CC1E) | (TIM_CCER_CC2E);
-
-  TIM1->SMCR &= ~(TIM_SMCR_TS);
-  TIM1->SMCR &= ~(TIM_SMCR_SMS);
-  TIM1->SMCR |= (TIM_SMCR_TS_2) | (TIM_SMCR_TS_0) | (TIM_SMCR_SMS_2);
-
-  TIM1->DIER |= TIM_DIER_CC1IE;
-  NVIC_EnableIRQ(TIM1_CC_IRQn);
-  TIM1->CR1 |= TIM_CR1_CEN;
-}
-
-void TIM1_CC_IRQHandler(void) {
-  printUSARTMOJ("interrupt");
-  if (TIM1->SR & TIM_SR_CC1IF) {
-    printUSARTMOJ("Period: %xw\nDuty C: %xw\n\n", TIM1->CCR1, TIM1->CCR2);
-    uint16_t current_bit = ambulance_id & (0x0001 << irq_counter);
-    if (TIM1->CCR1 >= PERIOD_MARGIN_LOW && TIM1->CCR1 <= PERIOD_MARGIN_HIGH &&
-        TIM1->CCR2 >= IR_MARGIN_0 && TIM1->CCR2 <= IR_MARGIN_2) {
-      if ((TIM1->CCR2 >= IR_MARGIN_1 && current_bit) ||
-          (TIM1->CCR2 <= IR_MARGIN_1 && !current_bit)) {
-        if (current_bit) {
-          pwm_recieved_bits |= (0x0001 << irq_counter);
-        }
-        irq_counter++;
-        // if (irq_counter == 16) {
-        //   printUSART2("GREAT SUCCESS!\n");
-        //   GPIOC->ODR |= 0x0004;
-        //   delay_ms(100);
-        //   irq_counter = 0;
-        //   pwm_cnt = 0;
-        // }
-      } else {
-        irq_counter = 0;
-        // pwm_recieved_bits = 0;
-      }
-    }
-  }
-
-  // Clear the interrupt flag
-  TIM1->SR &= ~TIM_SR_CC1IF;
 }
